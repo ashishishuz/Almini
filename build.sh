9b41cbd9c1f644c78669f096536fa6f0235f99fe
ashishishuz <118269397+ashishishuz@users.noreply.github.com>
2025-04-14 12:21:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 
-#!/bin/bash
 set -o errexit
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Change to project directory and run Django commands
 cd Almini
 python manage.py collectstatic --no-input
 python manage.py migrate
