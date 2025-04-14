228df5c69a1147199bf6a17041218aefb1085fc2
MyPathshala <ashishishu2002@gmail.com>
2025-04-14 12:17:50 +0530

    Update build.sh with proper formatting and permissions

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 
+#!/bin/bash
+set -o errexit
+
+# Install dependencies
 pip install -r requirements.txt
 
+# Change to project directory and run Django commands
 cd Almini
 python manage.py collectstatic --no-input
 python manage.py migrate
